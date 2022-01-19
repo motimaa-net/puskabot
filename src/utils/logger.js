03ccab67f9cb622161cf1018da666601c2f9d443
kassq <kasperi.pohtine@gmail.com>
2022-01-19 13:08:10 +0200

    Improved logger util. It now logs params of subcommand cmds correctly.

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -8,8 +8,17 @@ const { CommandInteraction } = require('discord.js');
 const logger = interaction => {
     const now = new Date();
     const commandArgs = [];
-    interaction.options.data.forEach(option => {
-        commandArgs.push(`(${option.name} = ${option.value})`);
+    const subCommand = interaction.options.data[0]?.type === 'SUB_COMMAND';
+    const member = interaction.options.getMember('käyttäjä');
+
+    const commandOptions = subCommand ? interaction.options.data[0].options : interaction.options.data;
+
+    commandOptions.forEach(option => {
+        if (option.name === 'käyttäjä' && member) {
+            commandArgs.push(`(${option.name}: ${member.user.tag} / ID: ${member.id})`);
+        } else {
+            commandArgs.push(`(${option.name} = ${option.value})`);
+        }
     });
 
     // Create a string of time in format HH:MM:SS. If the time is less than 10, add a 0 before it.
@@ -20,9 +29,9 @@ const logger = interaction => {
         .join(':');
 
     console.log(
-        `[${timeString}] ${interaction.user.username} suoritti komennon ${interaction.commandName} ${commandArgs.join(
-            ' ',
-        )}`,
+        `[${timeString}] ${interaction.user.username} suoritti komennon ${
+            subCommand ? `${interaction.commandName} ${interaction.options.getSubcommand()}` : interaction.commandName
+        } ${commandArgs.join(' ')}`,
     );
 };
 
